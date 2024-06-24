63627cce8ac0ca612cad173640c78382f1517bec
Martino Fontana <tinozzo123@gmail.com>
2024-06-24 14:57:34 +0200

    pcsx2: 1.7.5779 -> 1.7.5919
    Diff: https://github.com/PCSX2/pcsx2/compare/v1.7.5779...1.7.5919

--- pkgs/by-name/pc/pcsx2/package.nix
+++ pkgs/by-name/pc/pcsx2/package.nix
@@ -6,7 +6,6 @@
 , cubeb
 , curl
 , extra-cmake-modules
-, fetchpatch
 , ffmpeg
 , libaio
 , libbacktrace
@@ -33,8 +32,8 @@ let
   pcsx2_patches = fetchFromGitHub {
     owner = "PCSX2";
     repo = "pcsx2_patches";
-    rev = "b3a788e16ea12efac006cbbe1ece45b6b9b34326";
-    sha256 = "sha256-Uvpz2Gpj533Sr6wLruubZxssoXefQDey8GHIDKWhW3s=";
+    rev = "9e71956797332471010e563a4b75a5934bef9d4e";
+    sha256 = "sha256-jpaRpvJox78zRGyrVIGYVoSEo/ICBlBfw3dTMz9QGuU=";
   };
   inherit (qt6)
     qtbase
@@ -46,27 +45,18 @@ let
 in
 llvmPackages_17.stdenv.mkDerivation (finalAttrs: {
   pname = "pcsx2";
-  version = "1.7.5779";
+  version = "1.7.5919";
 
   src = fetchFromGitHub {
     owner = "PCSX2";
     repo = "pcsx2";
-    fetchSubmodules = true;
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-WiwnP5yoBy8bRLUPuCZ7z4nhIzrY8P29KS5ZjErM/A4=";
+    # NOTE: Don't forget to change the hash in shaderc-patched.nix as well.
+    sha256 = "sha256-cDugEbbz40uLPW64bcDGxfo1Y3ahYnEVaalfMp/J95s=";
   };
 
   patches = [
     ./define-rev.patch
-    # Backport patches to fix random crashes on startup
-    (fetchpatch {
-      url = "https://github.com/PCSX2/pcsx2/commit/e47bcf8d80df9a93201eefbaf169ec1a0673a833.patch";
-      sha256 = "sha256-7CL1Kpu+/JgtKIenn9rQKAs3A+oJ40W5XHlqSg77Q7Y=";
-    })
-    (fetchpatch {
-      url = "https://github.com/PCSX2/pcsx2/commit/92b707db994f821bccc35d6eef67727ea3ab496b.patch";
-      sha256 = "sha256-HWJ8KZAY/qBBotAJerZg6zi5QUHuTD51zKH1rAtZ3tc=";
-    })
   ];
 
   cmakeFlags = [
@@ -121,6 +111,7 @@ llvmPackages_17.stdenv.mkDerivation (finalAttrs: {
     let
       libs = lib.makeLibraryPath ([
         vulkan-loader
+        shaderc-patched
       ] ++ cubeb.passthru.backendLibs);
     in [
       "--prefix LD_LIBRARY_PATH : ${libs}"
